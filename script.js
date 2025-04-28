4b0f4b2b312c646911e3b32287c7fa1a0420865d
mxai0 <mahtabhasan3216@gmail.com>
2025-04-28 09:25:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,45 @@
-particlesJS("particles-js", {
+particlesJS('particles-js', {
   "particles": {
     "number": {
-      "value": 80,
+      "value": 120,
       "density": {
         "enable": true,
-        "value_area": 800
+        "value_area": 1000
       }
     },
-    "color": {
-      "value": "#00ffe1"
-    },
+    "color": { "value": "#00ffe1" },
     "shape": {
       "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      }
+      "stroke": { "width": 0, "color": "#000000" }
     },
     "opacity": {
-      "value": 0.5,
+      "value": 0.3,
       "random": true
     },
     "size": {
-      "value": 4,
+      "value": 3,
       "random": true
     },
     "line_linked": {
       "enable": true,
       "distance": 150,
       "color": "#0051ff",
-      "opacity": 0.4,
+      "opacity": 0.2,
       "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 2,
+      "speed": 1.5,
       "direction": "none",
-      "random": true,
-      "straight": false,
       "out_mode": "out"
     }
   },
   "interactivity": {
     "detect_on": "canvas",
     "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "grab"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      }
-    },
-    "modes": {
-      "grab": {
-        "distance": 140,
-        "line_linked": {
-          "opacity": 0.7
-        }
-      },
-      "push": {
-        "particles_nb": 4
-      }
+      "onhover": { "enable": true, "mode": "grab" },
+      "onclick": { "enable": true, "mode": "push" },
+      "resize": true
     }
   },
   "retina_detect": true
